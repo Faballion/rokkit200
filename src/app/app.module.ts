3194ab95a1a7f70782b9ecd05165a6b4fc805bbd
“Daniel” <faballion@gmail.com>
2024-02-22 16:34:49 +0200

    Pull mock data into service and create function skeleton with test cases

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AvailabilityCalendarComponent } from './availability-calendar/availability-calendar.component';
+import { AvailabilityService } from './availability.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AvailabilityCalendarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, AvailabilityCalendarComponent],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+    providers: [AvailabilityService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/availability-calendar/availability-calendar.component.ts
+++ src/app/availability-calendar/availability-calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AvailabilityService } from '../availability.service';
 
 @Component({
     selector: 'app-availability-calendar',
@@ -6,7 +7,17 @@ import { Component } from '@angular/core';
     styleUrl: './availability-calendar.component.scss'
 })
 export class AvailabilityCalendarComponent {
+    constructor(private availabilityService: AvailabilityService) {}
+
     ngOnInit(): void {
-        console.log('hello world');
+        this.availabilityService.getAvailabilityData().subscribe((data) => {
+            console.log('I have gotten the data!', data);
+        });
+
+        this.checkSlotAvailability(9, 1, '2016-05-20', [9, 10, 14, 15, 16, 17]); // => AVAILABLE
+        this.checkSlotAvailability(10, 1, '2016-05-20', [9, 10, 14, 15, 16, 17]); // => UNAVAILABLE
+        this.checkSlotAvailability(11, 1, '2016-05-20', [9, 10, 14, 15, 16, 17]); // => FULL
     }
+
+    checkSlotAvailability(time: number, jobLength: number, date: string, availability: number[]) {}
 }
